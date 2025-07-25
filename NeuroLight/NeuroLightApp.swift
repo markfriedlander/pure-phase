9ed76eb00240db5c616e9a6c8a6d0a4c1c3ad1c3
Mark Friedlander <markfriedlander@yahoo.com>
2025-07-24 21:25:45 -0700

    Initial Commit

---
+++ NeuroLight/NeuroLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroLightApp.swift
+//  NeuroLight
+//
+//  Created by Mark Friedlander on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NeuroLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
